cfb6c6fdc9b663169248a2a0d11fa21200429ca7
guigutan <345405123@qq.com>
2025-12-14 23:34:21 +0800

    修改后有误

--- src/api/LedStatus.ts
+++ src/api/LedStatus.ts
@@ -5,13 +5,10 @@ export const api = axios.create({
 });
 
 // SMOM 接口相关类型（和接口放一起）
-export interface CodeKSRow {
-  id: number;
-  barcode: string;
-  status: string;
-  create_time: string;
+export interface LedStatusRow {
+  MachineNO: string;
+  LedStatus: string;
 }
-
 // SMOM 条码查询接口
-export const queryCodeKS = (barcode: string) => 
-  api.post<{ success: boolean; data: CodeKSRow[] }>('/mes/codeKSMES', { barcode });
+export const LedStatus = (barcode: string) => 
+  api.post<{ success: boolean; data: LedStatusRow[] }>('/LedStatusRow', { barcode });
--- src/components/floor1.vue
+++ src/components/floor1.vue
@@ -2,13 +2,39 @@
     import {ref} from "vue";
     const mydata=[{MachineName:'C1' ,LedStatus: '1' }, {MachineName:'C2' ,LedStatus: '2' }, {MachineName:'C3' ,LedStatus: '3' }];
     const items = ref(mydata)
-    console.log(items);
+   
+
+    
+    import { LedStatus, type LedStatusRow } from '@/api/LedStatus';
+    const barcode = ref('');
+    const data = ref<LedStatusRow[]>([]);
+    const loading = ref(false);
+
+    const query = async () => {
+    loading.value = true;
+    try {
+        const res = await LedStatus(barcode.value);
+        if (res.data.success) {
+        data.value = res.data.data;
+        console.log(data);
+        } else {
+        alert('查询失败');
+        }
+    } catch (e) {
+        alert('网络错误');
+        console.error(e);
+    } finally {
+        loading.value = false;
+    }
+    };
 
 
 </script>
 
 <template>    
-    <div class="floorMsg">floorMsg</div>
+    <div class="floorMsg"><button @click="query" class="sc-button">查询</button>
+    <input v-model="barcode" placeholder="输入条码（支持模糊查询）" @keyup.enter="query" />
+</div>
     <div class="boxMachine">
      <!-- <div class="boxitem"> <img src="../assets/CNC80.png" class="MachineImg" alt=""> <div class="MachineName">C1</div></div> -->
    <div class="boxitem" v-for="info in items"> 
