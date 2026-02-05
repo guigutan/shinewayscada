a49ee881c9fc89d005ca76be3e4f865dd54ed7bf
guigutan <38305031+guigutan@users.noreply.github.com>
2026-02-05 10:53:31 +0800

    api域名转发

--- src/api/apiconfig.ts
+++ src/api/apiconfig.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 export const api = axios.create({
-  baseURL: 'http://api.shinewaygroup.com/api',
+  //baseURL: 'http://localhost:3000/api',
+  //baseURL: 'http://api.shinewaygroup.com/api',
+  baseURL: 'http://api.shinewaygroup.com:8000/api',
   timeout: 10000,
 });
 
